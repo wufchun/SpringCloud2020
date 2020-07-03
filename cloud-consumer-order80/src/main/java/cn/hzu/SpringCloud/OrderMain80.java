054845ef9a75bf1b0175e0a4571d3c0797c0de9a
wufchun <1462803989@qq.com>
2020-07-03 16:18:41 +0800

    eureka单机测试完成

--- cloud-consumer-order80/src/main/java/cn/hzu/SpringCloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/hzu/SpringCloud/OrderMain80.java
@@ -2,7 +2,9 @@ package cn.hzu.SpringCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 //@RibbonClient(name = "CLOUD-PROVIDER-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
--- cloud-eureka-server7001/src/main/java/cn/hzu/SpringCloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/cn/hzu/SpringCloud/EurekaMain7001.java
@@ -2,12 +2,14 @@ package cn.hzu.SpringCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//开启eureka的客户端
-@EnableEurekaClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//开启eureka服务端
+@EnableEurekaServer
 @SpringBootApplication
-public class PaymentMain8001 {
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
+
 }
